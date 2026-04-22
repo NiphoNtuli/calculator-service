4e5981f118763a84be99f4224b8cf9c06fd3e719
NiphoNtuli <78422655+NiphoNtuli@users.noreply.github.com>
2026-04-22 21:57:06 +0200

    Add CalculatorApplication class - 2026-04-22 19:56:01

--- src/main/java/com/niphontuli/CalculatorApplication.java
+++ src/main/java/com/niphontuli/CalculatorApplication.java
@@ -1,13 +1,9 @@
 package com.niphontuli;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CalculatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
-
 }
